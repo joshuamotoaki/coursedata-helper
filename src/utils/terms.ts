5fc887bcfee67077f80f9bb6cf3e1550995f5c93
Joshua Lau <dachshunduniverse@gmail.com>
2025-03-30 22:35:29 -0400

    Add documentation to terms

--- src/utils/terms.ts
+++ src/utils/terms.ts
@@ -1,3 +1,13 @@
+/**
+ * A list of term codes for Princeton University.
+ * Codes ending in 2 are fall terms, and codes ending in 4 are spring terms.
+ * For example, 1262 is the fall term of AY 2025-2026 (fall 2025),
+ * and 1254 is the spring term of AY 2024-2025 (spring 2025).
+ *
+ * There isn't any reason to believe the predictable pattern won't
+ * continue, but you should verify the term code before adding one
+ * for a new term when you go to update everything.
+ */
 export const TERMS = [
     // "1262", Not available
     // "1254", Not available
