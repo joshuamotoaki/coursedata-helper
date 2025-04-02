81c5472b143569828382f3e8b8919546378d5933
joshuamotoaki <dachshunduniverse@gmail.com>
2025-04-01 20:58:22 -0400

    Add comments to oitclient

--- src/clients/oitClient.ts
+++ src/clients/oitClient.ts
@@ -1,3 +1,5 @@
+// oitClient.ts
+// Author: Joshua Motoaki Lau
 import type { OitCourseDetails, OitDeptCourse, OitSeat, Status } from "../utils/oitTypes";
 
 /**
@@ -5,6 +7,12 @@ import type { OitCourseDetails, OitDeptCourse, OitSeat, Status } from "../utils/
  * data source for course information (with the exception of course IDs which
  * are more easily fetched from the registrar's public API and evaluations
  * which have to be web scraped, sadly).
+ *
+ * Information about courses is split between the /courses and /details
+ * endpoints (on the OIT's end), which reflects in the split between
+ * fetchDeptCourses and fetchCourseDetails. This split of information
+ * is wonky and quite frankly annoying, and you pretty much have to
+ * call both endpoints to get all the information you need about a course.
  *
  * This API is somewhat slow and its performance fluctuates heavily.
  * Do NOT use the API directly in an application. Instead, proxy the
@@ -30,10 +38,10 @@ export class OitClient {
     }
 
     /**
-     *
-     * @param dept
-     * @param term
-     * @returns
+     * Fetch the list of courses with main listing being in a given department
+     * @param dept 3-letter department code (e.g. "COS")
+     * @param term 4-digit term code (e.g. "1262" for Fall 2025)
+     * @returns A list of courses in the given department
      */
     public fetchDeptCourses = async (dept: string, term: string): Promise<OitDeptCourse[]> => {
         const rawDeptData = await fetch(
@@ -64,10 +72,10 @@ export class OitClient {
     };
 
     /**
-     *
-     * @param listingId
-     * @param term
-     * @returns
+     * Fetch the details for a given course listing
+     * @param listingId The course listing ID (e.g. "015230")
+     * @param term 4-digit term code (e.g. "1262" for Fall 2025)
+     * @returns The course details for the given listing ID
      */
     public fetchCourseDetails = async (
         listingId: string,
@@ -94,10 +102,12 @@ export class OitClient {
     };
 
     /**
-     *
-     * @param courseIds
-     * @param term
-     * @returns
+     * Fetch seat capacity information for a list of course IDs
+     * Note: This uses the OIT's accelerated API which means you
+     * can spam it to your heart's content (be nice though).
+     * @param courseIds A list of course IDs (e.g. ["015230", "017266"])
+     * @param term 4-digit term code (e.g. "1262" for Fall 2025)
+     * @returns A list of seat information for the given course IDs
      */
     public fetchSeats = async (courseIds: string[], term: string): Promise<OitSeat[]> => {
         const rawSeatData = await fetch(
